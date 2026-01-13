834212312bc72ca8a1633618b64d20fbb178e935
Taylor Barnes <taylor.a.barnes@gmail.com>
2026-01-13 13:46:56 -0500

    Add instructions

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Transpose a Matrix Across MPI Ranks
 
+In this problem, you will implement similar functionality to `MPI_Alltoall` using point-to-point communication calls (i.e. `MPI_Send`, `MPI_Irecv`, etc.).
+
+You will find starting code in `src/main.cpp`.
+This code creates a matrix in which each MPI rank stores a subset of rows in a matrix.
+As discussed in the lectures, an all-to-all operation effectively performs a transpose of a matrix.
+Write your code in the location indicated in `src/main.cpp`.
+Each rank has a vector named `my_partial_matrix`, which stores the elements of the matrix owned by that rank.
+The elements of the transposed matrix which are owned by each rank should be stored in the vector named `my_partial_transpose`.
+The code prints both the `my_partial_matrix` and `my_partial_transpose` vectors, so you should be able to confirm the validity of your implementation.
+
+You can compile and run the code with:
+
+```bash
+cmake -S . -B build
+cmake --build build
+```
+
+You can run the code with, for example:
+
+```bash
+mpiexec -n 4 ./build/transpose
+```
+
+Note that there is an overhead cost associated with every point-to-point communication call you make.
+In order to achieve reasonable efficiency, ensure that your implementation does not make an excessive number of point-to-point communication calls; in particular, the complete set of all ranks should make no more than a total of `2 * world_size * world_size` point-to-point communication calls.
+
+After performing the point-to-point transpose, perform the same operation using a call to `MPI_Alltoall`.
+Time your point-to-point transpose as well as the MPI_Alltoall for several different numbers of ranks.
+Report your result below.
+
+## Answer
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
+#include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <mpi.h>
+#include <thread>
 #include <vector>
 
 int main(int argc, char *argv[]) {
-  int N = 10; // Number of rows
-  int M = 10; // Number of columns
+  int N = 11; // Number of rows before transpose
+  int M = 5;  // Number of columns before transpose
 
   MPI_Init(&argc, &argv);
 
@@ -23,15 +26,75 @@ int main(int argc, char *argv[]) {
     if ( irank < N % world_size ) ++end_rows[irank];
   }
 
-  std::vector<std::vector<int>> my_partial_matrix(world_size, std::vector<int>(M));
+  std::vector<std::vector<int>> my_partial_matrix(end_rows[my_rank] - start_rows[my_rank], std::vector<int>(M));
   for (int irow = start_rows[my_rank]; irow < end_rows[my_rank]; ++irow) {
     for (int icolumn = 0; icolumn < M; ++icolumn) {
-      my_partial_matrix[irow - start_rows[my_rank]][icolumn] = icolumn + ( M * my_rank );
+      my_partial_matrix[irow - start_rows[my_rank]][icolumn] = icolumn + ( M * irow );
     }
   }
 
+  // Print the matrix
+  if ( my_rank == 0 ) {
+    std::cout << "Matrix before transpose: " << '\n';
+  }
+  for (int irank = 0; irank < world_size; ++irank) {
+    if ( my_rank == irank ) {
+      for (int irow = start_rows[my_rank]; irow < end_rows[my_rank]; ++irow) {
+        for (int icolumn = 0; icolumn < M; ++icolumn) {
+          std::cout << std::setw(3) << my_partial_matrix[irow - start_rows[my_rank]][icolumn] << " ";
+        }
+        std::cout << '\n';
+      }
+    }
+    // Note: MPI_Barrier does not ensure that I/O buffers have been written, so the result of this is technically undefined
+    // The call to sleep_for should largely ensure that the printing is well behaved
+    MPI_Barrier(MPI_COMM_WORLD);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+  if ( my_rank == 0 ) {
+    std::cout << '\n';
+  }
+
+  // In the transposed matrix, each rank will only store the set of rows from start_rows_transpose[my_rank] (inclusive) to end_rows_transpose[my_rank] (exclusive)
+  int start_rows_transpose[world_size];
+  int end_rows_transpose[world_size];
+  for (int irank = 0; irank < world_size; ++irank) {
+    if ( irank == 0 ) { start_rows_transpose[irank] = 0; }
+    else { start_rows_transpose[irank] = end_rows_transpose[irank - 1]; }
+    end_rows_transpose[irank] = start_rows_transpose[irank] + ( M / world_size );
+    if ( irank < M % world_size ) ++end_rows_transpose[irank];
+  }
+
+  // This vector should hold the transposed matrix
+  std::vector<std::vector<int>> my_partial_transpose(end_rows_transpose[my_rank] - start_rows_transpose[my_rank], std::vector<int>(N));
+
+
+
   // YOUR CODE GOES HERE
-  // Use point-to-point communication calls to transpose the matrix across ranks
+
+
+
+  // Print the transposed matrix
+  if ( my_rank == 0 ) {
+    std::cout << "Matrix after transpose: " << '\n';
+  }
+  for (int irank = 0; irank < world_size; ++irank) {
+    if ( my_rank == irank ) {
+      for (int irow = start_rows_transpose[my_rank]; irow < end_rows_transpose[my_rank]; ++irow) {
+        for (int icolumn = 0; icolumn < N; ++icolumn) {
+          std::cout << std::setw(3) << my_partial_transpose[irow - start_rows_transpose[my_rank]][icolumn] << " ";
+        }
+        std::cout << '\n';
+      }
+    }
+    // Note: MPI_Barrier does not ensure that I/O buffers have been written, so the result of this is technically undefined
+    // The call to sleep_for should largely ensure that the printing is well behaved
+    MPI_Barrier(MPI_COMM_WORLD);
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+  }
+  if ( my_rank == 0 ) {
+    std::cout << '\n';
+  }
 
   MPI_Finalize();
   return 0;
